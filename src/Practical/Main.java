df347903d5e9af9f30cef546750ae0470c982986
Labsession2 <duynkth1807054@fpt.edu.vn>
2019-04-05 08:44:02 +0700

    Initial commit

---
+++ src/Practical/Main.java
@@ -0,0 +1,12 @@
+package Practical;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String args[]){
+        Hotel hotel1 = new Hotel();
+        hotel1.inputInfo();
+        hotel1.printInfo();
+        Hotel hotel2 = new Hotel();
+    }
+}
